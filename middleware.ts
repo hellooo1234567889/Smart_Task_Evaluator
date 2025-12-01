47eb9e4293cf5ad739aacbae32774c2a30e5f17e
Pankssssssssss <148276304+hellooo1234567889@users.noreply.github.com>
2025-12-02 04:26:02 +0530

    refactor: update middleware config to specify route matchers for dashboard and auth

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,3 @@
-
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
@@ -51,7 +50,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/dashboard/:path*',
+    '/auth/:path*',
   ],
 }
-
