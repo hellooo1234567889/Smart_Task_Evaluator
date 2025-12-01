1e41b11dbe46e25b8b5317898a06118887f2e77c
Pankssssssssss <148276304+hellooo1234567889@users.noreply.github.com>
2025-12-02 03:47:36 +0530

    feat: add rewrites configuration to redirect all routes to index.html

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,14 @@ const nextConfig: NextConfig = {
       },
     ]
   },
+  async rewrites() {
+    return [
+      {
+        source: '/(.*)',
+        destination: '/index.html',
+      },
+    ]
+  },
 }
 
 export default nextConfig
